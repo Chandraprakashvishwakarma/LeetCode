a6956387b7cfb9aaccbbd9b88c8d48c08157f5a9
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-05-10 22:42:19 +0530

    Time: 1 ms (99.98%), Space: 43 MB (35.01%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
        List<List<Integer>> res = new ArrayList();
-        Arrays.sort(nums);
+        Arrays.sort(nums);//sorting is required so that we can check pre el and skip the iteration accordingly to skip the repetation
         helper(nums,res,new ArrayList<Integer>(),0);
         return res;
     }
     public void helper(int[] nums,List<List<Integer>> res, List<Integer> temp,int start){
         res.add(new ArrayList<>(temp));
         for(int i=start;i<nums.length;i++){
-            if(i!=start && nums[i]==nums[i-1]) continue;
+            if(i!=start && nums[i]==nums[i-1]) continue;//skipping the duplicate value
             temp.add(nums[i]);
             helper(nums,res,temp,i+1);
             temp.remove(temp.size()-1);
