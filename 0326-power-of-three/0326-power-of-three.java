348836a5cc572e8ec23652622bf0fc75b170d64d
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-05-28 15:45:19 +0530

    Time: 16 ms (40.82%), Space: 42.9 MB (12.23%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,9 +1,23 @@
-//using recursion 
+/*using recursion 
 class Solution {
     public boolean isPowerOfThree(int n) {
         if(n==1) return true;
         if(n<1) return false;
         if(n%3!=0) return false;
         return isPowerOfThree(n/3);
+    }
+}
+*/
+//using log properties
+/*
+X = 3^N;
+log10(X) = N*log10(3);
+N = log10(X)/log10(3);
+if N is integer then return true
+*/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<1) return false;
+        return (Math.log10(n)/Math.log10(3)%1 ==0);
     }
 }
