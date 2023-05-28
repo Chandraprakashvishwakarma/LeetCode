a9cb8abebd4d179e2cf3af4a465e559307426d02
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-05-28 15:27:46 +0530

    Time: 1 ms (100.00%), Space: 40 MB (52.24%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,19 @@
+//using recursion 
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n<1) return false;
+        if(n%2==1) return false;
+        
+        return isPowerOfTwo(n/2);
+    }
+}
+/*
+//O(1) solution without iteration or recursion
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        else return Integer.bitCount(n) == 1;
+        else return ((n&(n-1))==0);
     }
 }
+*/
