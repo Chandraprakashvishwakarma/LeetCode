15d467d1bdf02be5751a9778702c94df9eaddb03
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-11-05 22:47:45 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (18.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,18 +4,14 @@ Q. no. 17: Unique Paths
 class Solution {
     //Using formula 
     public int uniquePaths(int m, int n) {
-        int [][] dp = new int [m][n];
-        return findUnique(0,0,m-1,n-1,dp);
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        for(int i=1;i<=r;i++){
+            res = res*(N-r+i)/i;
+        }
+        return (int)res;
     }
-    public int findUnique(int i,int j,int m,int n,int [][]dp){
-        if(i==m && j == n) return 1;
-        if(i>m || j>n) return 0;
-        
-        if(dp[i][j]!=0) return dp[i][j];
-        
-        return dp[i][j] = findUnique(i+1,j,m,n,dp)+findUnique(i,j+1,m,n,dp);
-    }
-    
 }
 
 /*
