2384c40814c0ad7901052cc9d12aa4ba532e9ea9
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-11-08 00:31:34 +0530

    Time: 20 ms (82.54%), Space: 48.5 MB (18.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,24 +1,44 @@
 class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         int n1 = s1.length(),n2 = s2.length();
-        int [][] dp = new int[n1][n2];
-        for(int []row:dp) Arrays.fill(row,-1);
-        return LCS(s1,s2,s1.length()-1,s2.length()-1,dp);
+        int [][] dp = new int[n1+1][n2+1];
+        // for(int []row:dp) Arrays.fill(row,-1);
+        // return LCS(s1,s2,n1,n2,dp);
+        
+        //Tabulation
+        for(int j=0;j<n2;j++) dp[0][j]=0;
+        for(int i=0;i<n1;i++) dp[i][0]=0;
+            
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                int lcs = 0;
+                if(s1.charAt(i-1) == s2.charAt(j-1)){  //match condition
+                    lcs = 1 + dp[i-1][j-1];
+                }
+                else { //not match
+                    lcs = 0 + Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+                dp[i][j] = lcs;
+            }
+        }
+        
+        return dp[n1][n2];
     }
     
-    public int LCS(String s1,String s2,int ind1,int ind2,int [][] dp){
-        if(ind1<0 || ind2<0) return 0;
+    // Memoization 
+//     public int LCS(String s1,String s2,int i,int j,int [][] dp){
+//         if(i==0 || j==0) return 0;
         
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        int lcs=0;
-        if(s1.charAt(ind1) == s2.charAt(ind2)){  //match condition
-            lcs = 1 + LCS(s1,s2,ind1-1,ind2-1,dp);
-        }
-        else {
-            lcs = 0 + Math.max(LCS(s1,s2,ind1,ind2-1,dp),LCS(s1,s2,ind1-1,ind2,dp));
-        }
+//         int lcs=0;
+//         if(s1.charAt(i-1) == s2.charAt(j-1)){  //match condition
+//             lcs = 1 + LCS(s1,s2,i-1,j-1,dp);
+//         }
+//         else {
+//             lcs = 0 + Math.max(LCS(s1,s2,i,j-1,dp),LCS(s1,s2,i-1,j,dp));
+//         }
         
-        return dp[ind1][ind2] = lcs;
-    }
+//         return dp[i][j] = lcs;
+//     }
 }
