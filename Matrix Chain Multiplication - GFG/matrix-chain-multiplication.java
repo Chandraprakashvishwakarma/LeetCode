0f5e864a4bd8ca43712f3a4a7ecb91255ac3a436
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-11-13 18:13:23 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -31,22 +31,40 @@ class GFG
 class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
-        // code here
         int dp[][] = new int[N][N];
-        for(int [] row:dp) Arrays.fill(row,-1);
-        return f(arr,1,N-1,dp);
-    }
-    
-    static int f(int [] arr,int i,int j,int [][] dp){
-        if(i == j) return 0;
+        // for(int [] row:dp) Arrays.fill(row,-1);
+        // return f(arr,1,N-1,dp);
         
-        if(dp[i][j]!=-1) return dp[i][j];
         
-        int min = Integer.MAX_VALUE;
-        for(int k=i;k<j;k++){
-            int steps = (arr[i-1]*arr[k]*arr[j]) + f(arr,i,k,dp) + f(arr,k+1,j,dp);
-            min = Math.min(min,steps);
+        //Tabulation
+        //Base case
+        for(int i=0;i<N;i++) dp[i][i] = 0;
+        
+        for(int i=N-1 ;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int min = Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int steps = (arr[i-1]*arr[k]*arr[j]) + dp[i][k] + dp[k+1][j];
+                    min = Math.min(min,steps);
+                }
+                dp[i][j] = min;
+            }
         }
-        return dp[i][j] = min;
+        return dp[1][N-1];
+        
+        
     }
+    // Memoization
+    // static int f(int [] arr,int i,int j,int [][] dp){
+    //     if(i == j) return 0;
+        
+    //     if(dp[i][j]!=-1) return dp[i][j];
+        
+    //     int min = Integer.MAX_VALUE;
+    //     for(int k=i;k<j;k++){
+    //         int steps = (arr[i-1]*arr[k]*arr[j]) + f(arr,i,k,dp) + f(arr,k+1,j,dp);
+    //         min = Math.min(min,steps);
+    //     }
+    //     return dp[i][j] = min;
+    // }
 }
