a0fc8b1e0917769e7f765586596faef615748590
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-11-27 21:14:00 +0530

    Time: 2 ms (79.33%), Space: 43.9 MB (52.59%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,32 +3,31 @@ class Solution {
     int col[] = {1,0,-1,0};
     public void solve(char[][] board) {
         int n = board.length,m = board[0].length;
-        boolean [][] npos = new boolean[n][m];
         boolean [][] vis = new boolean[n][m];
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if((i==0 || j == 0 || i == n-1 || j == m-1) && board[i][j] == 'O' && !vis[i][j]){
-                    dfs(i,j,vis,board,npos);
-                }
-            }
+            if(!vis[i][0] && board[i][0] == 'O') dfs(i,0,vis,board);
+            if(!vis[i][m-1] && board[i][m-1] == 'O') dfs(i,m-1,vis,board);
+        }
+        for(int j=0;j<m;j++){
+            if(!vis[0][j] && board[0][j] == 'O') dfs(0,j,vis,board);
+            if(!vis[n-1][j] && board[n-1][j] == 'O') dfs(n-1,j,vis,board);
         }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == 'O' && !npos[i][j] ) board[i][j] = 'X';
+                if(board[i][j] == 'O' && !vis[i][j] ) board[i][j] = 'X';
             }
         }
     }
     
-    public void dfs(int i,int j,boolean [][] vis,char [][] board,boolean [][] npos){
+    public void dfs(int i,int j,boolean [][] vis,char [][] board){
         vis[i][j] = true;
-        npos[i][j] = true;
         for(int k = 0;k<4;k++){
             int r = i+row[k],c = j+col[k];
-            if(r>=0 && r<npos.length && c>=0 && c<npos[0].length 
+            if(r>=0 && r<vis.length && c>=0 && c<vis[0].length 
                && !vis[r][c] && board[r][c] == 'O'){
-                dfs(r,c,vis,board,npos);
+                dfs(r,c,vis,board);
             }
         }
     }
