33fb28a7abfaa61fc8c53592b54049204dc5f4cf
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-12-06 23:23:23 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (25.95%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,19 +1,18 @@
 class Solution {
     public int totalMoney(int n) {
-        int i=1;
-        int res = 0;
-        int days = 7;
-        while(n>0){
-            int j = i;
-            days = 7;
-            while(n>0 && days>0){
-                res+=j;
-                j++;
-                days--;
-                n--;
+        int w = n / 7;
+        int money = w * 28;
+        money += (7 * w * (w - 1)) / 2;
+
+        if (n % 7 != 0) {
+            int extraDays = n % 7;
+            int moneyToAdd = w + 1;
+            for (int i = 0; i < extraDays; ++i) {
+                money += moneyToAdd;
+                moneyToAdd += 1;
             }
-            i++;
         }
-        return res;
+
+        return money;
     }
 }
