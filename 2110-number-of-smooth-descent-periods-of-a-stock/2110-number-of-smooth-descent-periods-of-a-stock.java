15db0ff043f3c29d0d3d62f3782a32764b35a458
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-02-10 20:21:23 +0530

    Time: 2 ms (98.51%), Space: 60.6 MB (29.70%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -1,23 +1,18 @@
 class Solution {
     public long getDescentPeriods(int[] a) {
-        long ans = 0;
-        int dp[] = new int[a.length+1];
-        for(int i=0;i<a.length;i++){
-            if(dp[i]!=0) ans+=dp[i];
-            else ans+=fn(a,i,-1,new ArrayList<Integer>(),dp);
+        int end =0,start=0;
+        long ans=0;
+        int n = a.length;
+        while(end<n){
+            if(start == end ) {
+                ans++;end++;
+            }
+            else if(a[end]+1 == a[end-1]) {
+                ans = ans + (end - start + 1);
+                end++;
+            }
+            else start = end;
         }
         return ans;
-    }
-    
-    // dp =[0 0 0 0]
-    
-    public int fn(int [] a,int i,int pre,List<Integer> list,int [] dp){
-        if(i == a.length) return 0;
-        if(dp[i]!=0) return dp[i];
-        if(list.size()==0 || pre-a[i] == 1) {
-            list.add(a[i]);
-            return dp[i] = 1+fn(a,i+1,a[i],list,dp);
-        }
-        return 0;
     }
 }
