ccfd7577570586f522499f0d29bdd4e415bb54cd
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-04-02 11:42:41 +0530

    Time: 1 ms (24.89%), Space: 44.5 MB (43.86%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0,j=i+1;
+        int n=nums.length;
+        if(n<=2) return n;
+        int c=0;
+        int extraDuplicate = 0;
+        while(j<n-extraDuplicate){
+            if(nums[i]==nums[j]){
+                if(c>0){
+                    extraDuplicate++;
+                    shiftToLast(j,nums);
+                }
+                else{
+                    j++;
+                    c++;
+                }
+            }
+            else{
+                c=0;
+                i=j;
+                j=i+1;
+            }
+        }
+        return n-extraDuplicate;
+    }
+    public void shiftToLast(int j,int nums[]){
+        while(j<nums.length-1){
+            int temp = nums[j];
+            nums[j] = nums[j+1];
+            nums[j+1] = temp;
+            j++;
+        }
+    }
+}
