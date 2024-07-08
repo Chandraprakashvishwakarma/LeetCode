27a263cf1533f1ab7389dc79ff08b4bf0cb55b8e
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-07-09 01:24:25 +0530

    Time: 5 ms (57.50%), Space: 62.8 MB (51.34%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3208-alternating-groups-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3208-alternating-groups-ii.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3208-alternating-groups-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3208-alternating-groups-ii.java
@@ -1,23 +1,38 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] color, int k) {
+        
         int n = color.length;
-        int i=0,j=0;
-        int res=0;
-        int pre = -1;
-        while(i<n){
-            while(i<n && j-i<k){
-                if(color[j%n]==pre) {
-                    i=j;
-                    pre = -1;
-                }
-                else {
-                    pre = color[j%n];
-                    j++;
-                }
-            }
-            if(j-i==k) res++;
-            i++;
+        int count=0;
+        int res =0;
+        for(int i=1;i<n+k;i++){
+            if(color[i%n]!=color[(i-1)%n]) count++;
+            else count=1;
+            if(count>=k) res++;
         }
         return res;
+        
+        
+        
+        
+        //Sliding window solution
+        // int n = color.length;
+        // int i=0,j=0;
+        // int res=0;
+        // int pre = -1;
+        // while(i<n){
+        //     while(i<n && j-i<k){
+        //         if(color[j%n]==pre) {
+        //             i=j;
+        //             pre = -1;
+        //         }
+        //         else {
+        //             pre = color[j%n];
+        //             j++;
+        //         }
+        //     }
+        //     if(j-i==k) res++;
+        //     i++;
+        // }
+        // return res;
     }
 }
