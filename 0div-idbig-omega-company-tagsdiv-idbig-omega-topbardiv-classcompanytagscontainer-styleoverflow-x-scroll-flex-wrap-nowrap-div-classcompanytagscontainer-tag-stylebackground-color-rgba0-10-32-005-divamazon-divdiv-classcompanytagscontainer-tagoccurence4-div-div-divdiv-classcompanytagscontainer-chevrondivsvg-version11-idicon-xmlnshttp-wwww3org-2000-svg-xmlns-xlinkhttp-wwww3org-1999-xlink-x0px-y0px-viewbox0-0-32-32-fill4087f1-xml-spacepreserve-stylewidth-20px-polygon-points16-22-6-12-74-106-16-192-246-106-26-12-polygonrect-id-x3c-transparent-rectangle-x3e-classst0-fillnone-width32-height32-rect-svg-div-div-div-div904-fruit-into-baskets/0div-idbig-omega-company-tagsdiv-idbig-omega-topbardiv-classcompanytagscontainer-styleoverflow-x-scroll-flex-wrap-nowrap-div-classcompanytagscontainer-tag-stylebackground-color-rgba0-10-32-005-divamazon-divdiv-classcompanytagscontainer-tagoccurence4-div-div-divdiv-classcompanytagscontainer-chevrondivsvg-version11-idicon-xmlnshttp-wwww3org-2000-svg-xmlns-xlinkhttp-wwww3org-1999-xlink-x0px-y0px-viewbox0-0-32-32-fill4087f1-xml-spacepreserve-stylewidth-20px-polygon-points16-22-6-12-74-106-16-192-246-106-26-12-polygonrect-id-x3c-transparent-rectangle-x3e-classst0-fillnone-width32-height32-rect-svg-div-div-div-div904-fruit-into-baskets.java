533a6875933130886d0b90b73f8b17662ea8fee1
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-07-10 21:15:07 +0530

    Time: 6 ms (95.40%), Space: 54.6 MB (89.26%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div904-fruit-into-baskets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div904-fruit-into-baskets.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div904-fruit-into-baskets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div904-fruit-into-baskets.java
@@ -1,23 +1,28 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int n = fruits.length;
-        if(n<3) return n;
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        
-        int i=0,j=0,maxLen=0;
-        while(j<n){
-            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
-            while(map.size()>2){
-                map.put(fruits[i],map.get(fruits[i])-1);
-                if(map.get(fruits[i])==0){
-                    map.remove(fruits[i]);
-                }
-                i++;
-            }
-            maxLen=Math.max(maxLen,j-i+1);
-            j++;
+        if(fruits.length < 3) {
+            return fruits.length;
         }
-        return maxLen;
+        
+        int p1 = 0;
+        int p2 = 0;
+        int v1 = fruits[0];
+        int v2 = fruits[0];
+        int max = 1;
+        
+        for(int i = 1; i < fruits.length; i++) {
+            if(fruits[i] != v1 && fruits[i] != v2){
+                p1 = p2;
+                p2 = i;
+                v1 = fruits[i - 1];
+                v2 = fruits[i];
+            } else if(fruits[i] != fruits[i - 1]) {
+                p2 = i;
+            }
+            
+            max = Math.max(max, i - p1 + 1);
+        }
+        
+        return max;
     }
 }
