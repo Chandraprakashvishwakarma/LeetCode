8a3ccba0bd13e9e3cfee3f762d83e6dd6cff7dd2
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-07-12 22:36:12 +0530

    Time: 1 ms (99.72%), Space: 41.8 MB (51.70%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div809-expressive-words/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div809-expressive-words.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div809-expressive-words/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div809-expressive-words.java
@@ -1,22 +1,58 @@
+// class Solution {
+//     public int expressiveWords(String s, String[] words) {
+//         int res = 0;
+//         for(String t:words){
+//             int i=0,j=0;
+//             int m = s.length(),n = t.length();
+//             while(i<m && j<n){
+//                 int c1=0,c2=0;
+//                 while(i<m-1 && s.charAt(i) == s.charAt(i+1)){
+//                     i++;c1++;
+//                 }
+//                 while(j<n-1 && t.charAt(j) == t.charAt(j+1)){
+//                     j++;c2++;
+//                 }
+//                 if(s.charAt(i)!=t.charAt(j) || c1<c2 || (c1!=c2 && c1<2)) break;
+//                 i++;j++;
+//             }
+//             if(i == m && j == n) res++;
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
-    public int expressiveWords(String s, String[] words) {
-        int res = 0;
-        for(String t:words){
-            int i=0,j=0;
-            int m = s.length(),n = t.length();
-            while(i<m && j<n){
-                int c1=0,c2=0;
-                while(i<m-1 && s.charAt(i) == s.charAt(i+1)){
-                    i++;c1++;
-                }
-                while(j<n-1 && t.charAt(j) == t.charAt(j+1)){
-                    j++;c2++;
-                }
-                if(s.charAt(i)!=t.charAt(j) || c1<c2 || (c1!=c2 && c1<2)) break;
-                i++;j++;
-            }
-            if(i == m && j == n) res++;
-        }
-        return res;
+public int expressiveWords(String S, String[] words) {
+    int result = 0;
+    for (int i = 0; i < words.length; ++i) {
+        if (isStretchy(S, words[i])) result++;        
     }
+    return result;
+}
+
+private boolean isStretchy(String S, String word) {
+    int i = 0, j = 0;
+    while (i < S.length() && j < word.length()) {
+        if (S.charAt(i) != word.charAt(j)) return false;    // Characters don't match.
+        int sBlockSize = 1; 
+        i++;
+        // Count block size starting at S.charAt(i);
+        while (i < S.length() && S.charAt(i - 1) == S.charAt(i)) {
+            sBlockSize++;
+            i++;
+        }
+        int wordBlockSize = 1;
+        j++;
+        // Count block size starting at word.charAt(j);
+        while (j < word.length() && word.charAt(j - 1) == word.charAt(j)) {
+            wordBlockSize++;
+            j++;
+        }
+       
+        if (sBlockSize < wordBlockSize || (wordBlockSize < sBlockSize && sBlockSize < 3)) {
+            return false;         
+        }
+    }
+    return i == S.length() && j == word.length();   
+}
 }
