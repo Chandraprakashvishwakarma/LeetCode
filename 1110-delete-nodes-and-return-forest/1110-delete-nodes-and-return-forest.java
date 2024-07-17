83280e9b03f4b2d15e73de471289fbfe28b8b644
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-07-17 15:19:59 +0530

    Time: 1 ms (97.28%), Space: 44.8 MB (42.11%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -19,15 +19,15 @@ class Solution {
         for(int i:to_delete) set.add(i);
         List<TreeNode> res = new ArrayList<>();
         helper(root,res,set);
-        if(root!=null && !set.contains(root.val)) {
+        if(!set.contains(root.val)) {
             res.add(root);
         }
         return res;
     }
-    private void helper(TreeNode root,List<TreeNode> res,Set<Integer> set){
-        if(root == null) return;
-        helper(root.left,res,set);
-        helper(root.right,res,set);
+    private TreeNode helper(TreeNode root,List<TreeNode> res,Set<Integer> set){
+        if(root == null) return null;
+        root.left = helper(root.left,res,set);
+        root.right = helper(root.right,res,set);
         
         
         if(set.contains(root.val)){
@@ -37,8 +37,8 @@ class Solution {
             if(root.right!=null && !set.contains(root.right.val)) {
                 res.add(root.right);
             }
+            return null;
         }
-        if(root.left!=null && set.contains(root.left.val)) root.left = null;
-        if(root.right!=null && set.contains(root.right.val)) root.right = null;
+        return root;
     }
 }
