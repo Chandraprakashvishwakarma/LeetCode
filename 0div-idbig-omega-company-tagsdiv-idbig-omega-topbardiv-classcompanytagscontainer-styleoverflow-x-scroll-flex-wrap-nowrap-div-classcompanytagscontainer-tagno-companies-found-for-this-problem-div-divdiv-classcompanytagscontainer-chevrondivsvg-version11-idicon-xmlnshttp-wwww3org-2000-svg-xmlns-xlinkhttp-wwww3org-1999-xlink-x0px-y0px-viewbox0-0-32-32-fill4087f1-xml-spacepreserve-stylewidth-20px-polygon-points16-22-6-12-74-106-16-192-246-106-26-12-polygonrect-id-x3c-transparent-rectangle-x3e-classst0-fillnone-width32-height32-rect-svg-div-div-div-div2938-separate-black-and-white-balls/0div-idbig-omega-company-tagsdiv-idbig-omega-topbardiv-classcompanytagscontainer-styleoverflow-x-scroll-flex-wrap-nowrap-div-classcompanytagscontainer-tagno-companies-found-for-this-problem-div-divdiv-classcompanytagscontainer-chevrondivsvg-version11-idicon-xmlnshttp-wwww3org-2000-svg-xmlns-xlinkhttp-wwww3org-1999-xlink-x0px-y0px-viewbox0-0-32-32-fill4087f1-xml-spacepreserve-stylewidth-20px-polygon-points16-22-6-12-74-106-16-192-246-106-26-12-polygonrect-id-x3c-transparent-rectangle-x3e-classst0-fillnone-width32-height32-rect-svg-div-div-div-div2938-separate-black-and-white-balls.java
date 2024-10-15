1535762d0bdc46bff4b060b467f37b2cda9822ab
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-10-15 22:32:08 +0530

    Time: 7 ms (100.00%), Space: 45.2 MB (86.38%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2938-separate-black-and-white-balls/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2938-separate-black-and-white-balls.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2938-separate-black-and-white-balls/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2938-separate-black-and-white-balls.java
@@ -1,25 +1,11 @@
 class Solution {
     public long minimumSteps(String s) {
-        char [] a = s.toCharArray();
-        int i = s.length()-1,j = s.length()-1;
-        long ans = 0;
-        while(i>=0 && a[i]=='1') {i--;j--;}
-        while(i>=0 && j>=0){
-            while(i>=0 && a[i]!='1') i--;
-            while(j>=0 && a[j]!='0') j--;
-            
-            if(j<i) break;
-            if(i>=0 && j>=0 && a[i] == '1' && a[j] == '0'){
-                swap(i,j,a);
-                ans+=(i-j);
-            }
+        int swapsNeedsToPlaceZeroToLeft = 0;
+        long totalSwapsNeeded = 0;
+        for(char c:s.toCharArray()){
+            if(c=='1') swapsNeedsToPlaceZeroToLeft++;
+            else totalSwapsNeeded+=swapsNeedsToPlaceZeroToLeft;
         }
-        return Math.abs(ans);
-    }
-    
-    public void swap(int i,int j,char [] a){
-        char c = a[i];
-        a[i] = a[j];
-        a[j] = c;
+        return totalSwapsNeeded;
     }
 }
