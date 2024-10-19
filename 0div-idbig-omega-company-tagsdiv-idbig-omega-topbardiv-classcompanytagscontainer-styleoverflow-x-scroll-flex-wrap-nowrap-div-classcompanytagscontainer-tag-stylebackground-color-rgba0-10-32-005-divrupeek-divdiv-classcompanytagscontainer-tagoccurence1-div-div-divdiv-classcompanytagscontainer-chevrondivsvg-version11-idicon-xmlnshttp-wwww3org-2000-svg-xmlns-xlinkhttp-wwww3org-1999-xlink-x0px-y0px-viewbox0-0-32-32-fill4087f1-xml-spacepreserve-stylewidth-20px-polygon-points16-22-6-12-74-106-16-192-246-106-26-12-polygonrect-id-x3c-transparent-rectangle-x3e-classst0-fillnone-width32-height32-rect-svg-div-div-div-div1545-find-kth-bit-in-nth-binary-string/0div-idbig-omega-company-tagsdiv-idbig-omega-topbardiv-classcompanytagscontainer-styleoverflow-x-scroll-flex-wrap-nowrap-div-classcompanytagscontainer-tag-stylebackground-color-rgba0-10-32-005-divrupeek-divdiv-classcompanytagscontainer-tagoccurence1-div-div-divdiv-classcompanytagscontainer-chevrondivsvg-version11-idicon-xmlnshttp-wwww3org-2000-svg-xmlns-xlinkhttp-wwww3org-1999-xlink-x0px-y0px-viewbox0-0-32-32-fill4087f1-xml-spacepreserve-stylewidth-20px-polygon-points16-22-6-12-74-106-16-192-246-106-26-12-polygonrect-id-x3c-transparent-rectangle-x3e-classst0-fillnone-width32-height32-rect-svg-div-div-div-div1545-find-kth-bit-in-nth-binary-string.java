efc5cd8b6b98d49eeb9b915e5bcb1bb813ddb1e5
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2024-10-19 17:01:52 +0530

    Time: 87 ms (40.85%), Space: 51.4 MB (29.96%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divrupeek-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1545-find-kth-bit-in-nth-binary-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divrupeek-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1545-find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        StringBuilder sb = new StringBuilder("0");
+        for(int i=1;i<n;i++){
+            StringBuilder invertedAndReversed = invertAndReverse(new StringBuilder(sb));
+            sb.append('1');
+            sb.append(invertedAndReversed);
+        }
+        
+        return sb.charAt(k-1);
+    }
+    
+    private StringBuilder invertAndReverse(StringBuilder sb){
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i) == '0'){
+                sb.setCharAt(i,'1');
+            }
+            else {
+                sb.setCharAt(i,'0');
+            }
+        }
+        return sb.reverse();
+    }
+}
